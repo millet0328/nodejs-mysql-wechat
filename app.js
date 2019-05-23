c7922234198ee10045dda4400f09e30c6d894a57
silky <nn880328@126.com>
2019-05-23 17:22:21 +0800

    开启token验证

--- app.js
+++ app.js
@@ -20,13 +20,15 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //使用中间件验证token合法性
-// app.use(expressJwt({ secret: 'secret' }).unless({
-//     path: ['/api/user/token','/api/admin/register','/api/admin/login'] //除了这些地址，其他的URL都需要验证
-// }));
+app.use(expressJwt({ secret: 'secret' }).unless({
+  path: ['/api/user/token', '/api/admin/register', '/api/admin/login'] //除了这些地址，其他的URL都需要验证
+}));
 
 app.use('/api', index);
 app.use('/api', users);
