3560d80e35d9a209d9fce7f17254e5b02f897329
silky <nn880328@126.com>
2019-05-26 17:26:04 +0800

    增加admin端的商品列表、详情api

--- routes/admin.js
+++ routes/admin.js
@@ -878,6 +878,75 @@ router.post("/goods/edit", function(req, res) {
 	});
 });
 /**
+ * @api {get} /api/admin/goods/list 获取商品列表
+ * @apiDescription 具备商品分页功能，3个分类参数至多能传1个
+ * @apiName AdminGoodsList 获取商品列表
+ * @apiGroup Admin Goods
+ * @apiPermission admin
+ * 
+ * @apiParam {Number} [pageSize] 一个页有多少个商品,默认4个;
+ * @apiParam {Number} [pageIndex] 第几页,默认1;
+ * @apiParam {Number} [cate_1st] 一级分类id;
+ * @apiParam {Number} [cate_2nd] 二级分类id;
+ * @apiParam {Number} [cate_3rd] 三级分类id;
+ * @apiParam {String="ASC","DESC"} [sortByPrice] 按照价格排序，从小到大-ASC,从大到小-DESC;
+ * @apiSampleRequest /api/admin/goods/list
+ */
+router.get("/admin/goods/list", function(req, res) {
+	let query = req.query;
+	//取出空键值对
+	for (let key in query) {
+		if (!query[key]) {
+			delete query[key]
+		}
+	}
+	//拼接SQL
+	function produceSQL({ pageSize = 4, pageIndex = 1, sortByPrice = '', cate_1st = '', cate_2nd = '', cate_3rd = '', }) {
+		let size = parseInt(pageSize);
+		let count = size * (pageIndex - 1);
+		let sql = `SELECT id,name,price,img_md,articleNo,inventory,create_time FROM GOODS`
+		if (cate_1st) {
+			sql += `WHERE cate_1st = ${cate_1st}`;
+		}
+		if (cate_2nd) {
+			sql += `WHERE cate_2nd = ${cate_2nd}`;
+		}
+		if (cate_3rd) {
+			sql += `WHERE cate_3rd = ${cate_3rd}`;
+		}
+		sql += ` ORDER BY create_time DESC , price ${sortByPrice} LIMIT ${count},${size}`
+		return sql;
+	}
+	db.query(produceSQL(req.query), [], function(results, fields) {
+		//成功
+		res.json({
+			status: true,
+			msg: "success!",
+			data: results
+		});
+	});
+});
+/**
+ * @api {get} /api/admin/goods/detail/ 获取商品详情
+ * @apiName GoodsDetail
+ * @apiGroup Admin Goods
+ * 
+ * @apiParam {Number} id 商品id;
+ * 
+ * @apiSampleRequest /api/admin/goods/detail/
+ */
+router.get("/admin/goods/detail/", function(req, res) {
+	let sql = `SELECT * FROM GOODS WHERE id = ?`
+	db.query(sql, [req.query.id], function(results, fields) {
+		//成功
+		res.json({
+			status: true,
+			msg: "success!",
+			data: results[0]
+		});
+	});
+});
+/**
  * @api {post} /api/goods/delete/ 删除商品
  * @apiName goods/delete/
  * @apiGroup Admin Goods
