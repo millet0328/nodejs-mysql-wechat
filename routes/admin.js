4ae80eb6ec7801b8c07391984ea9426d4be02bea
silky <nn880328@126.com>
2019-06-02 18:26:59 +0800

    增加admin订单列表

--- routes/admin.js
+++ routes/admin.js
@@ -186,7 +186,7 @@ router.post('/admin/login', function(req, res) {
 router.get("/admin/list", function(req, res) {
 	//查询账户数据
 	let sql =
-		`SELECT a.id,a.username,a.nickname,a.sex,a.avatar,a.tel,r.role_name,r.id AS role FROM ADMIN AS a LEFT JOIN admin_role AS ar ON a.id = ar.admin_id LEFT JOIN role AS r ON r.id = ar.role_id`;
+		`SELECT a.id,a.username,a.nickname,a.sex,a.avatar,a.tel,r.role_name,r.id AS role FROM ADMIN AS a LEFT JOIN admin_role AS ar ON a.id = ar.admin_id LEFT JOIN role AS r ON r.id = ar.role_id ORDER BY a.id`;
 	db.query(sql, [], function(results, fields) {
 		if (!results.length) {
 			res.json({
@@ -200,6 +200,27 @@ router.get("/admin/list", function(req, res) {
 			status: true,
 			msg: "获取成功！",
 			data: results
+		});
+	})
+});
+/**
+ * @api {post} /api/admin/delete/ 删除admin用户
+ * @apiName DeleteAdmin
+ * @apiGroup Admin User
+ * @apiPermission admin
+ * 
+ * @apiParam {Number} uid admin用户id.
+ * 
+ * @apiSampleRequest /api/admin/delete
+ */
+router.post('/admin/delete', function(req, res) {
+	let { uid } = req.body;
+	let sql = `DELETE FROM admin WHERE id = ?;DELETE FROM admin_role WHERE admin_id = ?;`
+	db.query(sql, [uid, uid], function(results) {
+		// 获取成功
+		res.json({
+			status: true,
+			msg: "删除成功！",
 		});
 	})
 });
@@ -233,7 +254,7 @@ router.get("/admin/info", function(req, res) {
 	})
 });
 /**
- * @api { post } /api/admin/info/update/ 更新个人资料
+ * @api { post } /api/admin/info/update/ 更新admin个人资料
  * @apiName UpdateInfo
  * @apiGroup Admin User
  * 
@@ -1039,5 +1060,61 @@ router.post("/goods/delete", function(req, res) {
 		});
 	});
 });
-
+/**
+ * @api {post} /api/admin/order/list/ 获取所有账户订单列表
+ * @apiDescription 获取系统中的订单列表，根据订单状态获取列表，具备分页功能
+ * @apiName AdminOrderList
+ * @apiGroup Admin Order
+ * 
+ * @apiParam {Number} [pageSize] 一个页有多少个商品,默认4个;
+ * @apiParam {Number} [pageIndex] 第几页,默认1;
+ * @apiParam {Number=0,3,4,5,all} status 订单状态:0-待付款，3-待发货，4-待收货，5-待评价，all-所有状态;
+ * 
+ * @apiSampleRequest /api/admin/order/list/
+ */
+router.post('/admin/order/list/', function(req, res) {
+	let { pageSize = 4, pageIndex = 1, status = 'all', id } = req.body;
+	let size = parseInt(pageSize);
+	let count = size * (pageIndex - 1);
+	// 查询所有订单
+	let sql =
+		`SELECT o.id, o.create_time, o.payment, os.text AS status
+		 FROM orders o JOIN order_status os ON o.order_state = os.CODE LIMIT ? OFFSET ?`;
+	// 根据订单状态查询
+	if (status != 'all') {
+		sql =
+			`SELECT o.id, o.create_time, o.payment, os.text AS status
+			 FROM orders o JOIN order_status os ON o.order_state = os.CODE
+			 WHERE o.order_state = ${status} LIMIT ? OFFSET ?`;
+	}
+	db.query(sql, [size, count], function(results, fields) {
+		// 查询订单商品信息
+		let data = results;
+		let sql =
+			`SELECT g.id, o.id AS order_id, g.name, g.img_md, og.goods_num, og.goods_price
+			 FROM orders o JOIN order_goods og ON o.id = og.order_id
+			 JOIN goods g ON g.id = og.goods_id`;
+		if (status != 'all') {
+			sql += ` WHERE o.order_state = ${status}`;
+		}
+		db.query(sql, [], (results, fields) => {
+			data.forEach((order) => {
+				if (!order.goodsList) {
+					order.goodsList = [];
+				}
+				results.forEach((goods) => {
+					if (order.id == goods.order_id) {
+						order.goodsList.push(goods);
+					}
+				});
+			});
+			//成功
+			res.json({
+				status: true,
+				msg: "success!",
+				data
+			});
+		});
+	});
+});
 module.exports = router;
