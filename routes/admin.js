a465a98314d49356d8614179e5a98a507437d6b9
silky <nn880328@126.com>
2019-06-06 17:31:37 +0800

    增加admin后台

--- routes/admin.js
+++ routes/admin.js
@@ -754,7 +754,7 @@ router.post("/upload/editor", upload.single('file'), function(req, res) {
 	res.json({
 		errno: 0,
 		msg: "图片上传处理成功!",
-		data: [`${server.host}:3000` + fileFolder + filename + extName]
+		data: fileFolder + filename + extName
 	});
 });
 /**
