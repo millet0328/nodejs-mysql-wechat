3cade9db89d24fc5745e9b02a18f0b43a1b27521
moz <nn880328@126.com>
2019-07-25 08:01:51 +0800

    增加根据角色获取树形菜单

--- routes/admin/menu.js
+++ routes/admin/menu.js
@@ -94,5 +94,42 @@ router.get("/menu/sub/", function (req, res) {
         });
     });
 });
-
+/**
+ * @api {get} /api/menu/tree/ 根据角色id获取侧边栏树形菜单
+ * @apiName TreeMenu
+ * @apiGroup admin-Menu
+ * @apiPermission admin
+ *
+ * @apiParam {Number} id 角色id.
+ *
+ * @apiSampleRequest /api/menu/tree/
+ */
+router.get('/menu/tree', function (req, res) {
+    let {id} = req.query;
+    let sql = `SELECT m.* FROM MENU m JOIN role_menu rm ON rm.menu_id = m.id WHERE rm.role_id = ?`;
+    db.query(sql, [id], function (results) {
+        //筛选出一级菜单
+        let menu_1st = results.filter((item) => item.pId === 1 ? item : null);
+        //递归循环数据
+        parseToTree(menu_1st);
+        //递归函数
+        function parseToTree(array) {
+            array.forEach(function (parent) {
+                parent.children = [];
+                results.forEach(function (child) {
+                    if (child.pId === parent.id) {
+                        parent.children.push(child);
+                    }
+                });
+                parseToTree(parent.children);
+            });
+        }
+        //成功
+        res.json({
+            status: true,
+            msg: "success!",
+            data: menu_1st
+        });
+    });
+});
 module.exports = router;
