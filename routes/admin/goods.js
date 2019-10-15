885c5785a964c88e14bd16d9383fe237178a693f
moz <nn880328@126.com>
2019-10-15 23:33:04 +0800

    修改商品列表api，增加total总数；

--- routes/admin/goods.js
+++ routes/admin/goods.js
@@ -30,10 +30,12 @@ let db = require('../../config/mysql');
  * @apiSampleRequest /api/admin/goods
  */
 router.post("/", function (req, res) {
-    let {cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight} = req.body;
+    let { cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight } = req.body;
     let sql =
         `INSERT INTO GOODS (cate_1st,cate_2nd,cate_3rd,name,hotPoint,price,marketPrice,cost,discount,inventory,articleNo,img_lg,img_md,slider,brand,detail,freight,create_time) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP()) `;
-    db.query(sql, [cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight], function (results, fields) {
+    db.query(sql, [cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory,
+        articleNo, img_lg, img_md, slider, brand, detail, freight
+    ], function (results) {
         //成功
         res.json({
             status: true,
@@ -72,10 +74,12 @@ router.post("/", function (req, res) {
  * @apiSampleRequest /api/admin/goods
  */
 router.put("/", function (req, res) {
-    let {id, cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight} = req.body;
+    let { id, cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight } = req.body;
     let sql =
         `UPDATE GOODS SET cate_1st=?,cate_2nd=?,cate_3rd=?,name=?,hotPoint=?,price=?,marketPrice=?,cost=?,discount=?,inventory=?,articleNo=?,img_lg=?,img_md=?,slider=?,brand=?,detail=?,freight=?,update_time = CURRENT_TIMESTAMP() WHERE id=?`;
-    db.query(sql, [cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory, articleNo, img_lg, img_md, slider, brand, detail, freight, id], function (results, fields) {
+    db.query(sql, [cate_1st, cate_2nd, cate_3rd, name, hotPoint, price, marketPrice, cost, discount, inventory,
+        articleNo, img_lg, img_md, slider, brand, detail, freight, id
+    ], function (results) {
         //成功
         res.json({
             status: true,
@@ -86,52 +90,53 @@ router.put("/", function (req, res) {
 });
 /**
  * @api {get} /api/admin/goods/list 获取商品列表
- * @apiDescription 具备商品分页功能，3个分类参数至多能传1个
+ * @apiDescription 具备商品分页功能，3个分类id参数至多能传1个，默认按照商品创建时间升序排序
  * @apiName AdminGoodsList
  * @apiGroup admin Goods
  * @apiPermission admin
  *
- * @apiParam {Number} [pageSize] 一个页有多少个商品,默认4个;
- * @apiParam {Number} [pageIndex] 第几页,默认1;
+ * @apiParam {Number} [pageSize=4] 一个页有多少个商品;
+ * @apiParam {Number} [pageIndex=1] 第几页;
  * @apiParam {Number} [cate_1st] 一级分类id;
  * @apiParam {Number} [cate_2nd] 二级分类id;
  * @apiParam {Number} [cate_3rd] 三级分类id;
  * @apiParam {String="ASC","DESC"} [sortByPrice] 按照价格排序，从小到大-ASC,从大到小-DESC;
+ *
+ * @apiSuccess {Object[]} goods 商品数组.
+ * @apiSuccess {Number} total 商品总数.
+ * 
  * @apiSampleRequest /api/admin/goods/list
  */
 router.get("/list", function (req, res) {
-    let query = req.query;
-    //取出空键值对
-    for (let key in query) {
-        if (!query[key]) {
-            delete query[key]
-        }
-    }
-
+    let { pageSize = 4, pageIndex = 1, cate_1st, cate_2nd, cate_3rd, sortByPrice } = req.query;
     //拼接SQL
-    function produceSQL({pageSize = 4, pageIndex = 1, sortByPrice = '', cate_1st = '', cate_2nd = '', cate_3rd = '',}) {
-        let size = parseInt(pageSize);
-        let count = size * (pageIndex - 1);
-        let sql = `SELECT id,name,price,img_md,articleNo,inventory,create_time FROM GOODS`
-        if (cate_1st) {
-            sql += `WHERE cate_1st = ${cate_1st}`;
-        }
-        if (cate_2nd) {
-            sql += `WHERE cate_2nd = ${cate_2nd}`;
-        }
-        if (cate_3rd) {
-            sql += `WHERE cate_3rd = ${cate_3rd}`;
-        }
-        sql += ` ORDER BY price ${sortByPrice},create_time DESC LIMIT ${count},${size}`
-        return sql;
+    let size = parseInt(pageSize);
+    let count = size * (pageIndex - 1);
+    let sql =
+        `SELECT SQL_CALC_FOUND_ROWS id,name,price,img_md,articleNo,inventory,DATE_FORMAT(create_time,'%Y-%m-%d %H:%i:%s') AS create_time FROM GOODS`
+    if (cate_1st) {
+        sql += ` WHERE cate_1st = ${cate_1st}`;
     }
+    if (cate_2nd) {
+        sql += ` WHERE cate_2nd = ${cate_2nd}`;
+    }
+    if (cate_3rd) {
+        sql += ` WHERE cate_3rd = ${cate_3rd}`;
+    }
+    if (sortByPrice) {
+        sql += ` ORDER BY price ${sortByPrice}`;
+    } else {
+        sql += ` ORDER BY create_time DESC`;
+    }
+    sql += ` LIMIT ${count},${size};SELECT FOUND_ROWS() as total;`
 
-    db.query(produceSQL(req.query), [], function (results, fields) {
+    db.query(sql, [], function (results) {
         //成功
         res.json({
             status: true,
             msg: "success!",
-            data: results
+            goods: results[0],
+            ...results[1][0],
         });
     });
 });
@@ -145,9 +150,9 @@ router.get("/list", function (req, res) {
  * @apiSampleRequest /api/admin/goods
  */
 router.get("/", function (req, res) {
-    let {id} = req.query;
+    let { id } = req.query;
     let sql = `SELECT * FROM GOODS WHERE id = ?`;
-    db.query(sql, [id], function (results, fields) {
+    db.query(sql, [id], function (results) {
         //成功
         res.json({
             status: true,
@@ -167,9 +172,9 @@ router.get("/", function (req, res) {
  * @apiSampleRequest /api/admin/goods
  */
 router.delete("/", function (req, res) {
-    let {id} = req.query;
+    let { id } = req.query;
     let sql = `DELETE FROM GOODS WHERE id=?`;
-    db.query(sql, [id], function (results, fields) {
+    db.query(sql, [id], function (results) {
         //成功
         res.json({
             status: true,
