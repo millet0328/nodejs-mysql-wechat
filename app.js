0ce324ecd7077c94850b749f0a7ef51bcdf48c58
鞠祥明 <nn880328@126.com>
2020-04-26 15:29:46 +0800

    修改401返回的JSON格式

--- app.js
+++ app.js
@@ -74,11 +74,10 @@ app.use(function (err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({
       status: false,
-      msg: err,
+      ...err,
     });
   }
 });
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
